a425e1b50f6b254ae1211ddb6503b5c2a830ffe6
cpuCode <923992029@qq.com>
2021-11-18 13:58:22 +0800

    初始化

---
+++ HdfsClientDemo/src/main/java/com/cpucode/hdfs/HdfsClient.java
@@ -0,0 +1,10 @@
+package com.cpucode.hdfs;
+
+/**
+ * @author : cpucode
+ * @date : 2021/11/18 13:57
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class HdfsClient {
+}
