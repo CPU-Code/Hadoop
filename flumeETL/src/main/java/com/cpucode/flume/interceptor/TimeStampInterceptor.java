e7d024b99d234c57d9dd17411f4cf3a49edb56d8
cpuCode <923992029@qq.com>
2022-03-01 21:08:51 +0800

    消费者的解决零点漂移

---
+++ flumeETL/src/main/java/com/cpucode/flume/interceptor/TimeStampInterceptor.java
@@ -0,0 +1,61 @@
+package com.cpucode.flume.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @author : cpucode
+ * @date : 2022/3/1 19:53
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class TimeStampInterceptor implements Interceptor {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Event intercept(Event event) {
+        Map<String, String> headers = event.getHeaders();
+        String log = new String(event.getBody(), StandardCharsets.UTF_8);
+        JSONObject jsonObject = JSONObject.parseObject(log);
+        String ts = jsonObject.getString("ts");
+
+        headers.put("timestamp", ts);
+
+        return event;
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> list) {
+        for (Event event : list) {
+            intercept(event);
+        }
+        return list;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    public static class Builder implements Interceptor.Builder {
+        @Override
+        public Interceptor build() {
+            return new TimeStampInterceptor();
+        }
+
+        @Override
+        public void configure(Context context) {
+
+        }
+    }
+}
